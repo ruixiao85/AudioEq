9e8d748f41a71a5afb3d697685195d25b32d9f90
Rui Xiao <ruixiao85@gmail.com>
2023-04-09 21:39:24 -0700

    README update Download, Call script.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # AudioEq
+
+## Data Source Preparation
+
+https://github.com/jaakkopasanen/AutoEq
+
+[Download Script](download.bash): only download the frequency response data from the repo mentioned above.
+
+## Utility Scripts
+[Call Adjustment Script](call_adjust.bash)
+
+### Wavelet App
+```bash
+python3 foobar18eq.py \
+  -f "AKG K701" -f "Sennheiser HD 800" \
+  -t "AKG K812" -t "HIFIMAN HE1000 V2" \
+  -r 9 -l 4
+# list all the "from" & "to" targets, ratio=9: maximum adjustment, limit=4: 2^4=16, range (-16,+16) db
+
+python3 wavelet.py -f "Sennheiser HD 800" -o "wavelet_SnHD800" -r 9 -r 6
+# from one headphone to all others with 2 different adjustment ratios, and to a specific output directory
+```
+
+### Foobar2000 plugin: 18-band Equalizer
+
+### Foobar2000 plugin: 31-band Graphic Equalizer
--- call_adjust.bash
+++ call_adjust.bash
@@ -1,15 +1,18 @@
 
 python3 foobar18eq.py \
   -f "AKG K701" -f "Sennheiser IE 900 RX" -t "Sennheiser HD 800" -t "Audio-Technica ATH-W5000 2013" -t "AKG K812" -t "HIFIMAN HE1000 V2" \
-  -r 9 -l 5
+  -r 9 -l 4
 
 python3 foobar31geq.py \
   -f "AKG K701" -f "Sennheiser IE 900 RX" -t "Sennheiser HD 800" -t "Audio-Technica ATH-W5000 2013" -t "AKG K812" -t "HIFIMAN HE1000 V2" \
-  -r 9 -l 5
+  -r 9 -l 4
 
-python3 wavelet.py -f "Sennheiser HD 800"
-  # -r 9 -l 5
-python3 wavelet.py -f "Sennheiser IE 900 RX"
+python3 wavelet.py -f "Sennheiser HD 800" -o "wavelet_SnHD800"
+python3 wavelet.py -f "Sennheiser IE 900 RX" -o "wavelet_SnIE900RX"
+python3 wavelet.py -f "Anker Soundcore Space Q45" -o "wavelet_AkrQ45"
+python3 wavelet.py -f "Sennheiser IE 900" -o "wavelet_SnIE900"
+python3 wavelet.py -f "Audio-Technica ATH-W5000 2013" -o "wavelet_ATHW5000n"
+python3 wavelet.py -f "Audio-Technica ATH-W5000" -o "wavelet_ATHW5000"
 
 python3 wavelet.py \
   -f "Sennheiser HD 800" \
@@ -17,7 +20,7 @@ python3 wavelet.py \
   -f "Audio-Technica ATH-W5000" \
   -f "Anker Soundcore Space Q45" \
   -f "Sennheiser IE 900 RX" \
-  -r 6 -r 5 \
-  -l 15
+  -r 9 -r 5 \
+  -l 4
 
 
